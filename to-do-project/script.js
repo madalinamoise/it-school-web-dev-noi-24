6a855c0ec117dfc3d54b02831cd119b5cd5bf7e0
Madalina Moise <Madalina.Moise@endava.com>
2025-03-03 20:28:25 +0200

    blog project update

--- to-do-project/script.js
+++ to-do-project/script.js
@@ -2,7 +2,62 @@ const getTodos = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos');
   const todos = await response.json();
 
-  return todos.slice(0, 20);
+  return todos.slice(0, 40);
+};
+
+const getUser = async (id) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const user = await response.json();
+
+  return user;
+};
+
+const getUsersQueryParamString = (uniqueUserIds) => {
+  // [1,2] => 'userId=1&userId=2'
+
+  //solution 1
+  // let queryParamsString = '';
+  // uniqueUserIds.forEach((id, index) => {
+  //   if (index > 0) {
+  //     queryParamsString = queryParamsString + '&';
+  //   }
+
+  //   queryParamsString = queryParamsString + 'userId=' + id;
+  // });
+
+  //solution 2
+  return uniqueUserIds.reduce((acc, curr, currIndex) => {
+    if (currIndex > 0) {
+      acc = acc + '&';
+    }
+    acc = acc + 'id=' + curr;
+    return acc;
+  }, '');
+};
+
+const getUniqueUsers = async (users) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users?${getUsersQueryParamString(
+      users
+    )}`
+  );
+  return await response.json();
+};
+
+const getUsers = async (todos) => {
+  const uniqueUsers = todos.reduce((acc, curr) => {
+    if (!acc.find((el) => el === curr.userId)) {
+      acc.push(curr.userId);
+    }
+
+    return acc;
+  }, []);
+
+  // uniqueUsers.forEach(async (id) => await getUser(id));
+
+  await getUniqueUsers(uniqueUsers);
 };
 
 const splitTodos = (todos) => {
@@ -19,39 +74,41 @@ const splitTodos = (todos) => {
   };
 };
 
-const initialize = async () => {
-  const todos = await getTodos();
-  const { completed, progress } = splitTodos(todos);
+const generateTodosContent = (elementId, todos) => {
+  const listEl = document.getElementById(elementId);
 
-  //homework: refactor the following code
-  const progressListEl = document.getElementById('progress-list');
-
-  progress.forEach((todo) => {
+  todos.forEach((todo) => {
     const p = document.createElement('p');
     p.innerText = todo.title;
-    progressListEl.appendChild(p);
+    listEl.appendChild(p);
   });
-
-  const completedListEl = document.getElementById('completed-list');
-
-  completed.forEach((todo) => {
-    const p = document.createElement('p');
-    p.innerText = todo.title;
-    completedListEl.appendChild(p);
-  });
-
-  // const filteredTodos = todos.reduce(
-  //   (prev, curr) => {
-  //     const x = curr.completed
-  //       ? prev.completed.push(curr)
-  //       : prev.progress.push(curr);
-  //     console.log(x);
-  //     return x;
-  //   },
-  //   {
-  //     completed: [],
-  //     progress: [],
-  //   }
 };
 
-initialize();
+const displayTodos = (todos) => {
+  const { completed, progress } = splitTodos(todos);
+  generateTodosContent('progress-list', progress);
+  generateTodosContent('completed-list', completed);
+};
+
+const main = async () => {
+  const todos = await getTodos();
+  const users = await getUsers(todos);
+  displayTodos(todos);
+
+  // uniquesIDs = [1,2]
+};
+
+main();
+
+// const filteredTodos = todos.reduce(
+//   (prev, curr) => {
+//     const x = curr.completed
+//       ? prev.completed.push(curr)
+//       : prev.progress.push(curr);
+//     console.log(x);
+//     return x;
+//   },
+//   {
+//     completed: [],
+//     progress: [],
+//   }
